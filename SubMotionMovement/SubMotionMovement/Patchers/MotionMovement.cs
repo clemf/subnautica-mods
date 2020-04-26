02a9f7cf07ae16d11773a0f24ce4db0bb6d8e76b
Clem Freeman <clemfree@gmail.com>
2020-04-26 01:46:43 -0700

    Wow it works kind of

--- SubMotionMovement/SubMotionMovement/Patchers/MotionMovement.cs
+++ SubMotionMovement/SubMotionMovement/Patchers/MotionMovement.cs
@@ -3,6 +3,9 @@ using Harmony;
 using UnityEngine.XR;
 using System.Collections.Generic;
 using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Linq;
 
 namespace SubMotionMovement.Patchers
 {
@@ -27,6 +30,11 @@ namespace SubMotionMovement.Patchers
                 }
                 return _instance;
             }
+        }
+
+        public static Quaternion getHandRotation()
+        {
+            return Instance._leftHand.transform.rotation;
         }
 
         public void instantiate ()
@@ -73,7 +81,7 @@ namespace SubMotionMovement.Patchers
             mainCamera.transform.rotation = _cameraBackup;
         }
 
-        void getHandRotation ()
+        void updateHandRotation ()
         {
             Quaternion leftHandRotation;
             Vector3 leftHandPosition;
@@ -107,34 +115,78 @@ namespace SubMotionMovement.Patchers
 
         void Update()
         {
-            getHandRotation();
+            updateHandRotation();
         }
 
-        [HarmonyPatch(typeof(UnderwaterMotor), "UpdateMove")]
-        class UpdateMovePrefix
+        [HarmonyPatch(typeof(UnderwaterMotor))]
+        [HarmonyPatch("UpdateMove")]
+        public static class UnderwatermotorUpdateMove
         {
-            static void Prefix(UnderwaterMotor __instance)
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                if (!XRSettings.enabled)
+                MethodInfo getHandRotation = typeof(UnderwatermotorUpdateMove).GetMethod(nameof(UnderwatermotorUpdateMove.TryThis));
+                int startIndex = -1, endIndex = -1;
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)
                 {
-                    return;
+                    if (codes[i].opcode == OpCodes.Ldarg_0)
+                    {
+                        if (codes[i + 1].opcode == OpCodes.Ldfld && codes[i + 1].operand is FieldInfo fieldInfo && fieldInfo.Name == nameof(UnderwaterMotor.playerController))
+                        {
+                            if (codes[i + 2].opcode == OpCodes.Callvirt && codes[i + 2].operand is MethodInfo methodInfo1 && methodInfo1.Name == "get_forwardReference")
+                            {
+                                if (codes[i + 3].opcode == OpCodes.Callvirt && codes[i + 3].operand is MethodInfo methodInfo2 && methodInfo2.Name == "get_rotation")
+                                {
+                                    startIndex = i;
+                                    endIndex = i + 3;
+                                }
+                            }
+                        }
+                    }
                 }
-                Instance.backupCameraTransform();
-                Instance.setCameraToHand();
+                if (startIndex > -1 && endIndex > -1)
+                {
+                    // we cannot remove the first code of our range since some jump actually jumps to
+                    // it, so we replace it with a no-op instead of fixing that jump (easier).
+                    codes[startIndex].opcode = OpCodes.Nop;
+                    codes[startIndex + 1].opcode = OpCodes.Nop;
+                    codes[startIndex + 2].opcode = OpCodes.Nop;
+                    codes[endIndex].opcode = OpCodes.Call;
+                    codes[endIndex].operand = getHandRotation;
+                }
+                return codes.AsEnumerable();
+            }
+            public static Quaternion TryThis()
+            {
+                return MotionMovement.getHandRotation();
             }
         }
 
-        [HarmonyPatch(typeof(UnderwaterMotor), "UpdateMove")]
-        class UpdateMovePostfix
-        {
-            static void PostFix(UnderwaterMotor __instance)
-            {
-                if (!XRSettings.enabled)
-                {
-                    return;
-                }
-                Instance.restoreCamera();
-            }
-        }
+        //[HarmonyPatch(typeof(UnderwaterMotor), "UpdateMove")]
+        //class UpdateMovePrefix
+        //{
+        //    static void Prefix(UnderwaterMotor __instance)
+        //    {
+        //        if (!XRSettings.enabled)
+        //        {
+        //            return;
+        //        }
+        //        Instance.backupCameraTransform();
+        //        Instance.setCameraToHand();
+        //    }
+        //}
+
+        //[HarmonyPatch(typeof(UnderwaterMotor), "UpdateMove")]
+        //class UpdateMovePostfix
+        //{
+        //    static void PostFix(UnderwaterMotor __instance)
+        //    {
+        //        if (!XRSettings.enabled)
+        //        {
+        //            return;
+        //        }
+        //        Instance.restoreCamera();
+        //    }
+        //}
     }
 }
